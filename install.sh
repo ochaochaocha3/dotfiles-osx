a2a3bf13e045329c3fe69eddf51781db7cf1376e
ocha <ochaochaocha3@gmail.com>
2018-01-13 00:40:01 +0900

    install.sh: PWD環境変数を使用する
    pwdコマンドを呼び出す必要はない

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 for f in _*; do
-  ln -sf "$(pwd)/$f" "$HOME/${f/_/.}"
+  ln -sf "$PWD/$f" "$HOME/${f/_/.}"
 done
 
 BIN_DIR=$HOME/bin
 mkdir -p $BIN_DIR
-ln -sf "$(pwd)/install-ruby.sh" $BIN_DIR
+ln -sf "$PWD/install-ruby.sh" $BIN_DIR
 
 RBENV_DIR=$HOME/.rbenv
 mkdir -p $RBENV_DIR
-ln -sf "$(pwd)/default-gems" $RBENV_DIR
+ln -sf "$PWD/default-gems" $RBENV_DIR
 
 NEOVIM_DIR=$HOME/.config/nvim
 mkdir -p $NEOVIM_DIR
-ln -sf "$(pwd)/nvim/init.vim" $NEOVIM_DIR
-ln -sf "$(pwd)/nvim/plugins.toml" $NEOVIM_DIR
+ln -sf "$PWD/nvim/init.vim" $NEOVIM_DIR
+ln -sf "$PWD/nvim/plugins.toml" $NEOVIM_DIR
